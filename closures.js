ff8f5a4bbd701a9a06fd26dbab395dc19a4d7777
Bryan <Jbrewton88@gmail.com>
2019-06-11 19:14:47 -0500

    first commit

--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+let inner = outer()
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner()
 
 
 
@@ -53,6 +53,7 @@ function callFriend(name) {
 
 //Code Here
 
+var callJake = callFriend('Jake')
 
 
 ////////// PROBLEM 3 //////////
@@ -62,15 +63,21 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter(){
+  let num = 1
+  function inner(){
+    return num++;
+  }
+  return inner
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,10 +93,17 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+  
 
   return {
-
+    inc: function(){
+      value++;
+      return value
+    },
+    dec: function(){
+      value--;
+      return value
+    }
   };
 }
 
@@ -113,9 +127,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message(){
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,7 +160,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function(){
+      return privateMethod()
+    }
   };
 })();
 
@@ -162,7 +181,12 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(add){
+      return secret += add
+    },
+    takeAwayFromSecret: function(minus){
+      return secret-= minus
+    }
   };
 }
 
@@ -188,9 +212,12 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
+    (function(i){
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
+  })(i);
   }
+  return setTimeout
 }
 timeOutCounter();
